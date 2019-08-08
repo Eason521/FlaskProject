ddc3ee380fd578298a1ff8eb4c2274798c3563e0
Tanyxn <1946648105@qq.com>
2019-08-08 22:17:35 +0800

    api的初步使用

--- FlaskDirtory/FlaskDirtory/main.py
+++ FlaskDirtory/FlaskDirtory/main.py
@@ -5,6 +5,7 @@ from flask import session
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_wtf.csrf import CSRFProtect #CsrfProtect用于1.0以前版本
+from flask_restful import Resource,Api
 
 app=Flask(__name__)
 pymysql.install_as_MySQLdb()
@@ -22,7 +23,12 @@ pymysql.install_as_MySQLdb()
 app.config.from_object("config.DEBUG_CONFIG")
 
 csrf=CSRFProtect(app)   #csrf使用
+api=Api(app)   #关联接口和api插件
 
-
+class Hello(Resource): #处理请求的视图类
+    def get(self):
+        return {"hello":"world"}
 
 models = SQLAlchemy(app) #关联sqlalchemy和flask应用
+
+api.add_resource(Hello,"/API/") #注册api接口路由
