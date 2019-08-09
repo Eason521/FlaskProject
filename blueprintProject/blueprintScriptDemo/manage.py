c430f0cb845d2be8c45f227f5db7379a211fe6b2
Tanyxn <1946648105@qq.com>
2019-08-09 11:45:24 +0800

    manage.py中创建添加命令行命令

--- blueprintProject/blueprintScriptDemo/manage.py
+++ blueprintProject/blueprintScriptDemo/manage.py
@@ -12,14 +12,28 @@ simple_blueprint1 = Blueprint("simple_page1",__name__) #创建蓝图
 
 # # 蓝图路由和视图
 @simple_blueprint1.route("/")
-
 def index1():
     return "Hello world111111"
 app.register_blueprint(simple_blueprint1)
-manage = Manager(app) #app命令行序列化
+manager = Manager(app) #app命令行序列化
+
+@manager.command
+def hello(name = "creat_add_user"):
+    username = input("username :")
+    password = input("password :")
+    email = input("email :")
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
-    manage.run()
+    manager.run()
 
 """
 命令行启动
